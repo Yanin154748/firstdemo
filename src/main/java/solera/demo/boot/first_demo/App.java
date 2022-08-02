da23b4894658e9568ada8d64b14de1e1f382ec37
Yanin Almaguer <Yanin.Almaguer@solera.com>
2022-08-02 15:33:35 -0500

    added initial commit

---
+++ src/main/java/solera/demo/boot/first_demo/App.java
@@ -0,0 +1,13 @@
+package solera.demo.boot.first_demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
